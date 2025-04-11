37a80d9ac0711bf8b1643c556c2e09480b8330ba
eungyu123 <150225773+eungyu123@users.noreply.github.com>
2025-04-11 11:13:33 +0900

    tasklist

---
+++ .storybook/main.js
@@ -0,0 +1,27 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ["../src/components/**/*.stories.@(js|jsx)"],
+  staticDirs: ["../public"],
+
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+    "@chromatic-com/storybook"
+  ],
+
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+
+  docs: {
+    autodocs: true
+  },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
+};
+export default config;
